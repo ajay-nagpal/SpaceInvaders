3a7a2f77d88b6012e59265bbf65b99ec6468e406
ajay-nagpal <ajaynagpal.an64@gmail.com>
2026-08-05 01:31:50 +0530

    spaceship bullet

--- space_invader.py
+++ space_invader.py
@@ -34,6 +34,8 @@ class Spaceship(pygame.sprite.Sprite):
         self.rect.center = (x,y)
         self.health_start=health
         self.health_remaining=health
+        self.last_bullet_created=pygame.time.get_ticks()#initnially no bullet
+        self.bullet_freq=300 #in milliseconds, 0.5 sec
 
     def update(self):
         #set speed
@@ -50,6 +52,13 @@ class Spaceship(pygame.sprite.Sprite):
         if key[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT-40:
             self.rect.y += speed
 
+        #shoot
+        time_now=pygame.time.get_ticks()
+        if key[pygame.K_SPACE] and time_now - self.last_bullet_created > self.bullet_freq:
+            bullet = SpaceshipBullet(self.rect.centerx,self.rect.top)
+            spaceship_bullet_group.add(bullet)
+            self.last_bullet_created=time_now
+
         #draw healthbar
         pygame.draw.rect(screen,RED,(self.rect.x,self.rect.bottom,self.rect.width,5))
         #initnally complte bar will be green
@@ -58,9 +67,25 @@ class Spaceship(pygame.sprite.Sprite):
             pygame.draw.rect(screen,GREEN,(self.rect.x,self.rect.bottom,\
                 int(self.rect.width *(self.health_remaining/self.health_start)),5))
 
+#create bullet calss for spaceship bullets
+class SpaceshipBullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.image.load("img/spaceship_bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
 
-#spaceship instance
+    def update(self):
+        #move bullet up each iteration
+        self.rect.y -= 10
+        #remove bullet if it goes off screen
+        if self.rect.bottom < 0:
+            self.kill()
+
+#group instances
 spaceship_group = pygame.sprite.Group()
+spaceship_bullet_group = pygame.sprite.Group()
+
 spaceship = Spaceship(SCREEN_WIDTH//2, SCREEN_HEIGHT-80,3)
 spaceship_group.add(spaceship)
 
@@ -79,8 +104,12 @@ while run:
 
     #update spaceship
     spaceship_group.update()
+    #update bullets
+    spaceship_bullet_group.update()
     #draw spaceship
     spaceship_group.draw(screen)
+    #draw bullets
+    spaceship_bullet_group.draw(screen)
     #update display
     pygame.display.update()
 
